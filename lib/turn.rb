6e7ff9af105d2ec0aae203ba5b082d99176f05d5
Erica Lacy <ericalacy7@gmail.com>
2018-11-04 17:12:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,7 +16,7 @@ def display_board(board)
   puts  " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(board)
+def input_to_index(input)
   board == gets.strip
   index == board - 1
 end 
